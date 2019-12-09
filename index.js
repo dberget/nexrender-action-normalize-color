783001164e6ded2278799273b1d9f1388c61f2f0
davidberget@gmail.com <davidberget@gmail.com>
2019-12-09 15:21:38 -0600

    round

--- index.js
+++ index.js
@@ -2,16 +2,12 @@ const { name } = require("./package.json");
 const path = require("path");
 
 module.exports = (job, settings, options, type) => {
-  console.log(options.layers);
-
   return new Promise((resolve, reject) => {
     options.layers.forEach(asset => {
       const index = job.assets.findIndex(x => x.layerName == options.layers);
       const color = hexToRgb(job.assets[index].value);
 
       job.assets[index].value = color;
-
-      console.log(job.assets[index]);
     });
 
     resolve(job);
@@ -22,9 +18,9 @@ function hexToRgb(hex) {
   var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
   return result
     ? [
-        parseInt(result[1], 16) / 255,
-        parseInt(result[2], 16) / 255,
-        parseInt(result[3], 16) / 255
+        round(parseInt(result[1], 16) / 255, 2),
+        rount(parseInt(result[2], 16) / 255, 2),
+        round(parseInt(result[3], 16) / 255, 2)
       ]
     : null;
 }
